fe8ed58b7f0c7bbd51e5f40c2341b06463c32dd3
mujeeb <mujeebasariboss143@gmail.com>
2022-07-29 23:15:17 +0530

    user

--- utility/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A = open("intput.txt")
-B = A.read()
-print(B)'''
-file2 = open(r"C:\Users\Mujeeb\OneDrive\Desktop\python-project-Ansari-mujibuddin-au28\entities\New folder\input.txt","a")
-file1 = open("output.txt","w")
-maze1= file2.append()
-"""
